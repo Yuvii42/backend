8c18bb8c7e2f0a45dc3e980bb540b6f7105b2f1a
Yuvii42 <yuvviirajput@gmail.com>
2025-01-07 08:54:29 +0530

    Second Commit

--- hello.js
+++ hello.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+/*
+const math = require('./math')
+
+console.log(math.addFn(25, 25));
+console.log(math.subFn(25, 20))
+
+*/
+
+const math = require("./math")
+console.log("Math value is", math);
